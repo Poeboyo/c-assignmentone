24707c54e4801cd28fde129c93ca39b782aca46c
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-06-11 15:47:25 -0400

    Init and first exercise compelete

---
+++ jeffreyPoe_assignmentOne.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Creator Jeffrey Poe 
+//Assignment One
+// COP 2220 
+
+int main(){
+printf("Jeffrey Poe\n");
+printf("Jeffrey \nPoe\n");
+printf("Jeffrey ");
+printf("Poe");
+
+};
